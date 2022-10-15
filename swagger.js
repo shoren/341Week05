244b199bb06c7b2574b9d8bc8f4eddefb4845e99
shoren <65180338+shoren@users.noreply.github.com>
2022-10-15 16:38:10 -0600

    render still borken

--- swagger.js
+++ swagger.js
@@ -1,13 +1,13 @@
 const swaggerAutogen = require('swagger-autogen')();
-// const appConfig = require("./config/app");
+const appConfig = require("./config/app");
 
 const doc = {
   info: {
     title: 'API for Week05',
     description: 'Week05 API',
   },
-  host: 'localhost:8081',
-  schemes: ['http'],
+  host: 'appConfig.host',
+  schemes: [appConfig.protocol],
 };
 
 const outputFile = './swagger.json';
