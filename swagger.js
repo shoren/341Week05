e3eb5a5e5de5bc62f62f7889618f911b2bf263f3
shoren <65180338+shoren@users.noreply.github.com>
2022-10-15 15:21:51 -0600

    adding gloves

--- swagger.js
+++ swagger.js
@@ -1,17 +1,17 @@
 const swaggerAutogen = require('swagger-autogen')();
-//const appConfig = require("./config/app");
+// const appConfig = require("./config/app");
 
 const doc = {
   info: {
     title: 'API for Week05',
     description: 'Week05 API',
   },
-  host: 'appConfig.host',
-  schemes: [appConfig.protocol],
+  host: 'localhost:8081',
+  schemes: ['http'],
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const endpointsFiles = ['./server.js'];
 
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
