23a52d1f6845376fea3e39091b186370cf706980
shoren <65180338+shoren@users.noreply.github.com>
2022-10-22 17:47:18 -0600

    week06 starting

--- controller/characters.js
+++ controller/characters.js
@@ -2,22 +2,32 @@ const { response } = require('express');
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 // GET fdsaf
-const getAllCharacters = async (req, res, next) => { //Have to put the name of the Database and the collection that belongs to the database respectively
+const getAllCharacters = async (req, res, next) => {
+  try {
     const result = await mongodb.getDb().db('Week05').collection('characters').find();
     result.toArray().then((lists) => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists);
     });
-  };
+  } catch (error) {
+    next(error);
+    // res.status(500).send({
+    //   message: error.message || 'Some error occurred while retrieving users.'})
+    };        //Have to put the name of the Database and the collection that belongs to the database respectively 
+  };  //testing here come back to this
 
   // GET
   const getSingleCharacters = async (req, res, next) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db('Week05').collection('characters').find({ _id: userId }); //Find
-    result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists[0]);
+    try {
+      const userId = new ObjectId(req.params.id);
+      const result = await mongodb.getDb().db('Week05').collection('characters').find({ _id: userId }); //Find
+      result.toArray().then((lists) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists[0]);
     });
+    } catch (error) {
+      next(error);
+    }
   };
 
   //POST working!
@@ -26,6 +36,10 @@ const getAllCharacters = async (req, res, next) => { //Have to put the name of t
       Name: req.body.Name,
       Ascendancy: req.body.Ascendancy,
       LeagueStart: req.body.LeagueStart
+
+      // const { ingredient } = req.query
+      // const ingredient = req.query.ingredient    
+      // These two lines are equivalent but the top one is shorter 
     };
     const result = await mongodb.getDb().db('Week05').collection('characters').insertOne(character); //insertOne
     if(result.acknowledged){
@@ -44,7 +58,12 @@ const getAllCharacters = async (req, res, next) => { //Have to put the name of t
       LeagueStart: req.body.LeagueStart
     };
 
-    const result = await mongodb.getDb().db('Week05').collection('characters').replaceOne({ _id: userId },character); //replaceOne
+    const result = await mongodb.getDb().db('Week05').collection('characters').replaceOne({ _id: userId },character);//replaceOne
+    try {
+      
+    } catch (error) {
+      
+    }
     if(result.acknowledged){
       res.status(204).send();
     }
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { application } = require('express');
 const express = require('express');
 const router = express.Router();
 
@@ -6,5 +7,19 @@ router.use('/characters',require('./characters'));
 router.use('/gloves',require('./gloves'));
 router.use('/api-docs', require('./docs'));
 router.use([openCors, express.json()]);
+
+app.use(
+    (error,req,res,next) =>{
+        res.status(400);
+
+        const statusMap = {
+            ValidationError: 400,
+        };
+
+        res.status(statusMap[error.name] || 500);
+
+        res.json({ error: error.message});
+    }
+);
 
 module.exports = router;
