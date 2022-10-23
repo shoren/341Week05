955805ce60c3711fcd3b50e0b07d664133db3411
shoren <65180338+shoren@users.noreply.github.com>
2022-10-22 20:43:24 -0600

    added validation and error codes

--- controller/characters.js
+++ controller/characters.js
@@ -1,6 +1,8 @@
 const { response } = require('express');
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
+
+
 // GET fdsaf
 const getAllCharacters = async (req, res, next) => {
   try {
@@ -20,13 +22,17 @@ const getAllCharacters = async (req, res, next) => {
   const getSingleCharacters = async (req, res, next) => {
     try {
       const userId = new ObjectId(req.params.id);
+      if(!userId){
+        res.status(400).send({message: "Please provide a Character ID"});
+        return;
+      }
       const result = await mongodb.getDb().db('Week05').collection('characters').find({ _id: userId }); //Find
       result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(lists[0]);
     });
     } catch (error) {
-      res.status(422).json("invalid input");
+      res.status(422).json("Character not found");
     }
   };
 
@@ -36,16 +42,21 @@ const getAllCharacters = async (req, res, next) => {
       Name: req.body.Name,
       Ascendancy: req.body.Ascendancy,
       LeagueStart: req.body.LeagueStart
-
-      // const { ingredient } = req.query
-      // const ingredient = req.query.ingredient    
-      // These two lines are equivalent but the top one is shorter 
+      // const { ingredient } = req.query || const ingredient = req.query.ingredient  These two are equivalent but the left one is shorter 
     };
-    const result = await mongodb.getDb().db('Week05').collection('characters').insertOne(character); //insertOne
-    if(result.acknowledged){
-      res.status(201).json(response);
-    } else {
-      res.status(500).json(response.error || 'Some error occurred while creating the character.');
+    try {
+      if(!Name || !Ascendancy || !LeagueStart){ // error validation
+        res.status(400).send({message: "Please fill out all parts of the form"});
+        return;
+      }
+      const result = await mongodb.getDb().db('Week05').collection('characters').insertOne(character); //insertOne
+      if(result.acknowledged){
+        res.status(201).json(response);
+      } else {
+        res.status(500).json('Unable to create character at this time.');
+    }
+    } catch (error) {
+      res.status(500).json('Unable to create character at this time.');
     }
   };
 
@@ -56,25 +67,34 @@ const getAllCharacters = async (req, res, next) => {
       Name: req.body.Name,
       Ascendancy: req.body.Ascendancy,
       LeagueStart: req.body.LeagueStart
-    };
-
-    const result = await mongodb.getDb().db('Week05').collection('characters').replaceOne({ _id: userId },character);//replaceOne
+    }
     try {
-      
+      if(!Name || !Ascendancy || !LeagueStart){ // error validation
+        res.status(400).send({message: "Please fill out all parts of the form"});
+        return;
+      }
+      const result = await mongodb.getDb().db('Week05').collection('characters').replaceOne({ _id: userId },character);//replaceOne
+      if(result.acknowledged){
+        res.status(204).send();
+      }
     } catch (error) {
-      
-    }
-    if(result.acknowledged){
-      res.status(204).send();
+      res.status(500).json('Unable to update character at this time.');
     }
   };
 
-  // DELETE  works
+  // DELETE  works similar to create, just check for id in errors
   const deleteCharacter = async (req, res, next) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db('Week05').collection('characters').remove({ _id: userId },true); // Remove
-    console.log(result);
-    res.status(200).send();
-  };
+    try {const userId = new ObjectId(req.params.id);
+      if(!userId){ // error validation
+        res.status(400).send({message: "Character ID  cannot be found"});
+        return;
+      }
+      const result = await mongodb.getDb().db('Week05').collection('characters').remove({ _id: userId },true); // Remove
+      console.log(result);
+      res.status(200).send();
+    } catch (error) {
+      res.status(500).json('Unable to delete character at this time.');
+    }};
+    
   
   module.exports = { getAllCharacters,getSingleCharacters,createCharacter,updateCharacter,deleteCharacter };
