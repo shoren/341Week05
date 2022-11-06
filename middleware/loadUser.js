811865b5bc65c0bf5cce0e04b84ad35020425fae
shoren <65180338+shoren@users.noreply.github.com>
2022-11-05 22:17:17 -0600

    bedtime

--- middleware/loadUser.js
+++ middleware/loadUser.js
@@ -4,6 +4,8 @@ const User = require("../models/user");
 const loadUser = async (req,res,next) =>{
     console.log(req.headers.authorization);
     const authZeroUser = await fetchAuthZeroUser(req.headers.authorization);
+    const user = await findOrCreateUser(authZeroUser);
+    req.user = user;
     next();
 };
 
@@ -15,10 +17,22 @@ const fetchAuthZeroUser = async (authorizationValue) =>{
     return response.json();
 };
 
-const findOrCreateUser = (authZeroUserJson) => {
+const findOrCreateUser = async (authZeroUserJson) => {
     if(!authZeroUserJson) return;
 
-    const existingUser = User
+    const existingUser =  await User.findOne({identifer: authZeroUserJson.sub});
+
+    if (existingUser) return existingUser;
+
+    const newUser =  await User.create({
+        identifier: authZeroUserJson.sub,
+        email: authZeroUserJson.email,
+        givenName: authZeroUserJson.givenName,
+        familyName: authZeroUserJson.familyName,
+        locale: authZeroUserJson.locale,
+        picture: authZeroUserJson.picture,
+    });
+    return newUser;
 
 }
 
